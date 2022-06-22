83cd71796674795439b35564dd1d63b77b9b2df3
Bruno Negri <b.negri_@hotmail.com>
2022-06-22 18:16:06 -0300

    Terminando a 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+import LOGIN from './actionTypes';
+
+const userLogin = (value) => ({
+  type: LOGIN,
+  payload: value,
+});
+
+export default userLogin;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,97 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import userLogin from '../actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    disableBtn: true,
+  }
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.enableBtn();
+  };
+
+  handleClick() {
+    const { history, dispatchLogin } = this.props;
+    const { email } = this.state;
+
+    dispatchLogin(email);
+    history.push('/carteira');
+
+    // console.log('clicou');
+  }
+
+  enableBtn() {
+    const { email, password } = this.state;
+    const numPassword = 5;
+    const emailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordValidate = password.length >= numPassword;
+    const validate = emailValidate.test(email) && passwordValidate;
+    // console.log(validate);
+    this.setState({
+      disableBtn: !validate,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    console.log(this.props);
+    const { email, password, disableBtn } = this.state;
+    return (
+      <div>
+        <div>
+          <div>
+            <span>EMAIL</span>
+            <input
+              data-testid="email-input"
+              name="email"
+              onChange={ this.handleChange }
+              type="email"
+              value={ email }
+              placeholder="email@example.com"
+            />
+          </div>
+          <div>
+            <span>SENHA </span>
+            <input
+              data-testid="password-input"
+              name="password"
+              onChange={ this.handleChange }
+              type="text"
+              value={ password }
+              placeholder="******"
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={ this.handleClick }
+            disabled={ disableBtn }
+          >
+            Entrar
+
+          </button>
+        </div>
+      </div>);
   }
 }
 
-export default Login;
+Login.defaultProps = {
+  history: {},
+};
+
+Login.propTypes = {
+  dispatchLogin: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(PropTypes.any),
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogin: (email) => dispatch(userLogin(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
