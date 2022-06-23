95e8f06b710543c603f62b763ec2a3f596568f53
Bruno Negri <b.negri_@hotmail.com>
2022-06-23 09:18:13 -0300

    Configurando CSS

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-import './App.css';
-
 function App() {
   return (
     <div>
