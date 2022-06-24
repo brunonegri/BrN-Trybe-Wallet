65aef1a4f8de8edf3e9764844dc12315b04d5bad
Bruno Negri <b.negri_@hotmail.com>
2022-06-24 03:09:31 -0300

    repair css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { userLogin } from '../actions';
-import '../css/Login.css';
+import '../css/index.css';
 
 class Login extends React.Component {
   state = {
